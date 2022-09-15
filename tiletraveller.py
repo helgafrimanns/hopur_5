425ad4b953d4561341bb83c1999a37cceb3094be
Helga Lind Pétursdóttir <helgalind23@gmail.com>
2022-09-15 14:20:23 +0000

    reitur 1,1

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,3 +1,17 @@
 #leikreglur
 
 """Leikurinn virkar þannig að ..."""
+
+
+
+print("You can travel (N)orth.")
+travel = input("Direction: ")
+
+def direction(travel):
+    if (travel == "n"):
+        return "You can travel (N)orth."
+    else:
+        return "Not a valid direction!"
+        
+print(direction(travel))
+    
