f925d66ca3d89045a35e985d88f66ae1ce6be472
Helga Lind Pétursdóttir <helgalind23@gmail.com>
2022-09-15 16:03:29 +0000

    engin timi

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,22 +1,58 @@
 #leikreglur
 
-"""Leikurinn virkar þannig að ..."""
+"""
 
+"""
 
-
-print("You can travel (N)orth.")
-travel = input("Direction: ")
+NORTH = 'n'
+SOUTH = 's'
+WEST = 'w'
+EAST = 'e'
 
 x = 1
 y = 1
 
-def move(x,y):
+def move(travel):
     if (travel == "n") or (travel == "N"):
-        y + 1
-        x = x
-        return "You can travel (N)orth or (E)ast or (S)outh."
-    else:
-        return "Not a valid direction!"
-               
-print(move(x,y))
+        return x, y + 1
+    elif (travel == "s") or (travel == "S"):
+        return x, y - 1
+    elif (travel == "w") or (travel == "W"):
+        return x -1, y
+    elif (travel == "e") or (travel == "E"):
+        return x + 1, y
+
+
+def valid_directions(x,y):
+    valid_directions = '' 
+    if x == 1 and y == 1:
+        valid_directions = NORTH
+    elif x == 1 and y == 2:
+        valid_directions = NORTH + EAST + SOUTH
+    elif x == 1 and y == 3: 
+        valid_directions = SOUTH + EAST
+    elif x == 2 and y == 3:
+        valid_directions = EAST + WEST
+    elif x == 2 and y == 2:
+        valid_directions = WEST + SOUTH
+    elif x == 2 and y == 1: 
+        valid_directions = NORTH
+    elif x == 3 and y == 3:
+        valid_directions = WEST + SOUTH
+    elif x == 3 and y == 2:
+        valid_directions = NORTH + SOUTH
+    elif x == 3 and y == 1:
+        valid_directions = NORTH
+    return valid_directions
+    
+def main():    
+    pick_a_direction = input("Direction: ").lower()
+    while pick_a_direction == "n" or pick_a_direction == "s" or pick_a_direction == "e" or pick_a_direction == "w":
+
+        pick_a_direction = input("Direction: ").lower()
+
+print(valid_directions(x,y))
+
+if __name__ == "__main__":
+    main()
     
