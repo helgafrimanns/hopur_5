75db494a8fb2055b60b47d6ec682e43c202c98fe
Helga Lind Pétursdóttir <helgalind23@gmail.com>
2022-09-15 14:43:47 +0000

    x og y

--- tiletraveller.py
+++ tiletraveller.py
@@ -7,11 +7,16 @@
 print("You can travel (N)orth.")
 travel = input("Direction: ")
 
-def direction(travel):
-    if (travel == "n"):
-        return "You can travel (N)orth."
+x = 1
+y = 1
+
+def move(x,y):
+    if (travel == "n") or (travel == "N"):
+        y + 1
+        x = x
+        return "You can travel (N)orth or (E)ast or (S)outh."
     else:
         return "Not a valid direction!"
-        
-print(direction(travel))
+               
+print(move(x,y))
     
