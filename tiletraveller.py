baad1899ca8fd2c0ae67b7c53fb6a5c608a08075
Helga <helbfri@gmail.com>
2022-09-16 09:58:01 +0000

    leikurinn....

--- tiletraveller.py
+++ tiletraveller.py
@@ -4,13 +4,17 @@
 
 """
 
+
 NORTH = 'n'
 SOUTH = 's'
 WEST = 'w'
 EAST = 'e'
 
+
 x = 1
 y = 1
+travel = ''
+valid_directions = ('')
 
 def move(travel):
     if (travel == "n") or (travel == "N"):
@@ -23,36 +27,52 @@ def move(travel):
         return x + 1, y
 
 
-def valid_directions(x,y):
-    valid_directions = '' 
-    if x == 1 and y == 1:
-        valid_directions = NORTH
-    elif x == 1 and y == 2:
-        valid_directions = NORTH + EAST + SOUTH
-    elif x == 1 and y == 3: 
-        valid_directions = SOUTH + EAST
-    elif x == 2 and y == 3:
-        valid_directions = EAST + WEST
-    elif x == 2 and y == 2:
-        valid_directions = WEST + SOUTH
-    elif x == 2 and y == 1: 
-        valid_directions = NORTH
-    elif x == 3 and y == 3:
-        valid_directions = WEST + SOUTH
-    elif x == 3 and y == 2:
-        valid_directions = NORTH + SOUTH
-    elif x == 3 and y == 1:
-        valid_directions = NORTH
-    return valid_directions
-    
-def main():    
-    pick_a_direction = input("Direction: ").lower()
-    while pick_a_direction == "n" or pick_a_direction == "s" or pick_a_direction == "e" or pick_a_direction == "w":
+while True:
+    if travel in valid_directions:
+        if x == 1 and y == 1:
+            valid_directions = NORTH 
+            print("You can travel: (N)orth.")
+        elif x == 1 and y == 2:
+            valid_directions = NORTH + EAST + SOUTH
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
+        elif x == 1 and y == 3:
+            valid_directions = SOUTH + EAST
+            print("You can travel: (E)ast or (S)outh.")
+        elif x == 2 and y == 3:
+            valid_directions = EAST + WEST
+            print("You can travel: (E)ast or (W)est.")
+        elif x == 2 and y == 2:
+            valid_directions = WEST + SOUTH
+            print("You can travel: (S)outh or (W)est.")
+        elif x == 2 and y == 1:
+            valid_directions = NORTH
+            print("You can travel: (N)orth.")
+        elif x == 3 and y == 3:
+            valid_directions = WEST + SOUTH
+            print("You can travel: (S)outh or (W)est.")
+        elif x == 3 and y == 2:
+            valid_directions = NORTH + SOUTH
+            print("You can travel: (N)orth or (S)outh.")
+        elif x == 3 and y == 1:
+            valid_directions = NORTH
+            print("Victory!")
+            exit()
 
-        pick_a_direction = input("Direction: ").lower()
+    travel = input("Direction: ").lower()
 
-print(valid_directions(x,y))
-
-if __name__ == "__main__":
-    main()
-    
+    if travel in valid_directions:
+        x, y = move(travel)
+    else:
+        print ('Not a valid direction!')
+        if valid_directions == "n":
+            print("You can travel: (N)orth.")
+        elif valid_directions == "nes":
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
+        elif valid_directions == "se":
+            print("You can travel: (E)ast or (S)outh.")
+        elif valid_directions == "ew":
+            print("You can travel: (E)ast or (W)est.")
+        elif valid_directions == "ws":
+            print("You can travel: (S)outh or (W)est.")
+        elif valid_directions == "ns":
+            print("You can travel: (N)orth or (S)outh.")
